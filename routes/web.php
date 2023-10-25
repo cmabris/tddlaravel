53549fcd57a81edfe7a6c465b4480e84ffe7b749
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2023-10-25 20:48:13 +0200

    Paso 3

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,27 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/usuarios', function () {
+    return 'Este es el listado de usuarios';
+});
+
+Route::get('usuarios/detalles', function () {
+    return 'Mostrando los detalles del usuario: ' . $_GET['id'];
+});
+
+Route::get('usuarios/{id}', function ($id) {
+    return 'Mostrando los detalles del usuario: ' . $id;
+})->where('id', '[0-9]+');
+
+Route::get('usuarios/nuevo', function () {
+    return 'Creando un usuario nuevo';
+});
+
+Route::get('saludo/{name}/{nickname?}', function ($name, $nickname = null) {
+    if ($nickname) {
+        return 'Bienvenido ' . $name . ', tu apodo es ' . $nickname;
+    } else {
+        return 'Bienvenido ' . $name . ', no tienes apodo';
+    }
+});
