afaacdf5a5acde5e40da3dd4806801a20af56e35
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2023-11-16 16:35:47 +0100

    Paso 28

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -7,6 +7,16 @@
 
     <form action="{{ route('user.store') }}" method="POST">
         {{ csrf_field() }}
+
+        <label for="name">Nombre</label>
+        <input type="text" name="name" placeholder="Nombre">
+        <br>
+        <label for="email">Correo Electrónico</label>
+        <input type="email" name="email" placeholder="email">
+        <br>
+        <label for="password">Contraseña</label>
+        <input type="password" name="password" placeholder="Al menos 6 caracteres">
+
         <button type="submit">Crear usuario</button>
     </form>
 
