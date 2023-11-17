4d9b81129f9104a1b28c72d89b1c992a2123627d
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2023-11-17 19:12:48 +0100

    Paso 34

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -52,7 +52,11 @@ class UserController extends Controller
 
     public function update(User $user)
     {
-        $data = request()->all();
+        $data = request()->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required',
+        ]);
 
         $data['password'] = bcrypt($data['password']);
 
