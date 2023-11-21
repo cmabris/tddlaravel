2bda337eee842136051c62192371ad33a46bbc32
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2023-11-21 19:26:48 +0100

    Paso 40. Fin curso inicial

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -3,35 +3,40 @@
 @section('title', 'Nuevo usuario')
 
 @section('content')
-    <h1>Crear nuevo usuario</h1>
-
-    @if($errors->any())
-        <div class="alert alert-danger">
-            <h6>Por favor, corrige los siguientes errores</h6>
-            <ul>
-                @foreach($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
+    <div class="card">
+        <div class="card-header h4">
+            Crear nuevo usuario
         </div>
-    @endif
+        <div class="card-body">
+            @if($errors->any())
+                <div class="alert alert-danger">
+                    <h6>Por favor, corrige los siguientes errores</h6>
+                    <ul>
+                        @foreach($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
 
-    <form action="{{ route('user.store') }}" method="POST">
-        {{ csrf_field() }}
+                <form action="{{ route('user.store') }}" method="POST">
+                    {{ csrf_field() }}
 
-        <label for="name">Nombre</label>
-        <input type="text" name="name" placeholder="Nombre" value="{{ old('name') }}">
-        <br>
-        <label for="email">Correo Electrónico</label>
-        <input type="email" name="email" placeholder="email" value="{{ old('email') }}">
-        <br>
-        <label for="password">Contraseña</label>
-        <input type="password" name="password" placeholder="Al menos 6 caracteres">
-
-        <button type="submit">Crear usuario</button>
-    </form>
-
-    <p>
-        <a href="{{ route('users.index') }}">Regresar al listado de usuarios</a>
-    </p>
+                    <div class="form-group">
+                        <label for="name">Nombre</label>
+                        <input type="text" name="name" placeholder="Nombre" value="{{ old('name') }}" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="email">Correo Electrónico</label>
+                        <input type="email" name="email" placeholder="email" value="{{ old('email') }}" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Contraseña</label>
+                        <input type="password" name="password" placeholder="Al menos 6 caracteres" class="form-control">
+                    </div>
+                    <button type="submit">Crear usuario</button>
+                    <a href="{{ route('users.index') }}" class="btn btn-link">Regresar al listado de usuarios</a>
+                </form>
+        </div>
+    </div>
 @endsection
