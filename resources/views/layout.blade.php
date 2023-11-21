9e2474fd476785b0ba7fa9fab950edf6217c1c79
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2023-11-21 19:21:08 +0100

    Paso 39

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -12,7 +12,8 @@
 
     <!-- Bootstrap core CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
-
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
 
     <!-- Favicons -->
     <link rel="apple-touch-icon" href="/docs/4.6/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -3,24 +3,45 @@
 @section('title', "Listado usuarios")
 
 @section('content')
-<h1>{{ $title }}</h1>
-<p>
-    <a href="{{ route('users.create') }}">Nuevo usuario</a>
-</p>
+    <div class="d-flex justify-content-between align-items-end mb-3">
+        <h1 class="pb-1">{{ $title }}</h1>
+        <p>
+            <a href="{{ route('users.create') }}" class="btn btn-primary">Nuevo usuario</a>
+        </p>
+    </div>
+
 <ul>
-    @forelse($users as $user)
-        <li>
-            {{ $user->name }} ({{ $user->email }})
-            <a href="{{ route('users.show', $user) }}">Ver Detalles</a>
-            <a href="{{ route('user.edit', $user) }}">Editar </a>
-            <form action="{{ route('user.destroy', $user) }}" method="POST">
-                {{ csrf_field() }}
-                {{ method_field('DELETE') }}
-                <button type="submit">Eliminar</button>
-            </form>
-        </li>
-    @empty
+    @if($users->isNotEmpty())
+        <table class="table">
+            <thead class="thead-dark">
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Nombre</th>
+                <th scope="col">Correo</th>
+                <th scope="col">Acciones</th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach($users as $user)
+                <tr>
+                    <td scope="row">{{ $user->id }}</td>
+                    <td>{{ $user->name }}</td>
+                    <td>{{ $user->email }}</td>
+                    <td class="form-inline">
+                        <a href="{{ route('users.show', $user) }}" class="btn btn-link"><span class="material-symbols-outlined">visibility</span></a>
+                        <a href="{{ route('user.edit', $user) }}" class="btn btn-link"><span class="material-symbols-outlined">edit</span></a>
+                        <form action="{{ route('user.destroy', $user) }}" method="POST">
+                            {{ csrf_field() }}
+                            {{ method_field('DELETE') }}
+                            <button type="submit" class="btn btn-link"><span class="material-symbols-outlined">delete</span></button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    @else
         <p>No hay usuarios registrados</p>
-    @endforelse
+    @endif
 </ul>
 @endsection
