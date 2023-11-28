dd36a2c64f89898a55db88c208d89a341044801c
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2023-11-28 20:31:21 +0100

    Paso 12

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -3,23 +3,19 @@
 @section('title', 'Nuevo usuario')
 
 @section('content')
-    <div class="card">
-        <div class="card-header h4">
-            Crear nuevo usuario
+    @card
+    @slot('header', 'Crear nuevo usuario')
+
+    @include('shared._errors')
+
+    <form action="{{ route('user.store') }}" method="POST">
+
+        @include('users._fields')
+
+        <div class="form-group mt-4">
+            <button type="submit">Crear usuario</button>
+            <a href="{{ route('users.index') }}" class="btn btn-link">Regresar al listado de usuarios</a>
         </div>
-        <div class="card-body">
-
-            @include('shared._errors')
-
-            <form action="{{ route('user.store') }}" method="POST">
-
-                @include('users._fields')
-
-                <div class="form-group mt-4">
-                    <button type="submit">Crear usuario</button>
-                    <a href="{{ route('users.index') }}" class="btn btn-link">Regresar al listado de usuarios</a>
-                </div>
-            </form>
-        </div>
-    </div>
+    </form>
+    @endcard
 @endsection
