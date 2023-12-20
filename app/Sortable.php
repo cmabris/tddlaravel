1c2f4dca01c8470c87cb2afff75d7cfe4c3639b0
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2023-12-20 20:17:25 +0100

    Paso 43

--- app/Sortable.php
+++ app/Sortable.php
@@ -2,10 +2,18 @@
 
 namespace App;
 
+use Illuminate\Support\Arr;
+
 class Sortable
 {
     protected $currentColumn;
     protected $currentDirection;
+    protected $currentUrl;
+
+    public function __construct($currentUrl)
+    {
+        $this->currentUrl = $currentUrl;
+    }
 
     public function setCurrentOrder($column, $direction = 'asc')
     {
@@ -13,13 +21,32 @@ class Sortable
         $this->currentDirection = $direction;
     }
 
-    public function classes($column)
+    public function url($column)
     {
         if ($this->currentColumn == $column && $this->currentDirection == 'asc') {
+            return $this->buildSortableUrl($column, 'desc');
+        }
+
+        return $this->buildSortableUrl($column);
+    }
+
+    protected function buildSortableUrl($column, $direction = 'asc')
+    {
+        return $this->currentUrl.'?'.Arr::query(['order' => $column, 'direction' => $direction]);
+    }
+
+    protected function isSortingBy($column, $direction)
+    {
+        return $this->currentColumn == $column && $this->currentDirection == $direction;
+    }
+
+    public function classes($column)
+    {
+        if ($this->isSortingBy($column, 'asc')) {
             return 'link-sortable link-sorted-up';
         }
 
-        if ($this->currentColumn == $column && $this->currentDirection == 'desc') {
+        if ($this->isSortingBy($column, 'desc')) {
             return 'link-sortable link-sorted-down';
         }
 
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -25,9 +25,17 @@
                 <thead class="thead-dark">
                 <tr>
                     <th scope="col"># <span class="oi oi-caret-bottom"></span><span class="oi oi-caret-top"></span></th>
-                    <th scope="col"><a href="#" class="{{ $sortable->classes('name') }}">Nombre</a></th>
-                    <th scope="col"><a href="#" class="{{ $sortable->classes('email') }}">Correo</a></th>
-                    <th scope="col"><a href="#" class="{{ $sortable->classes('created_at') }}">Fechas</a></th>
+                    <th scope="col"><a
+                                href="{{ $sortable->url('name') }}"
+                                class="{{ $sortable->classes('name') }}">Nombre</a>
+                    </th>
+                    <th scope="col"><a
+                                href="{{ $sortable->url('email') }}"
+                                class="{{ $sortable->classes('email') }}">Correo</a>
+                    </th>
+                    <th scope="col"><a
+                                href="{{ $sortable->url('created_at') }}"
+                                class="{{ $sortable->classes('created_at') }}">Fechas</a></th>
                     <th scope="col" class="text-right th-actions">Acciones</th>
                 </tr>
                 </thead>
