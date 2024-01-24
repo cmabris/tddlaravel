4fca7370157dd6d5d7f440cccc3a6361ed9c0e24
Carlos M Abrisqueta Valcárcel <cmabris@gmail.com>
2024-01-24 17:26:17 +0100

    Inicio de examen

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -3,19 +3,19 @@
 @section('title', 'Nuevo usuario')
 
 @section('content')
-    @card
-    @slot('header', 'Crear nuevo usuario')
+    @component('shared._card')
+        @slot('header', 'Crear nuevo usuario')
 
-    @include('shared._errors')
+        @include('shared._errors')
 
-    <form action="{{ route('user.store') }}" method="POST">
+        <form action="{{ route('user.store') }}" method="POST">
 
-        @include('users._fields')
+            @include('users._fields')
 
-        <div class="form-group mt-4">
-            <button type="submit">Crear usuario</button>
-            <a href="{{ route('users.index') }}" class="btn btn-link">Regresar al listado de usuarios</a>
-        </div>
-    </form>
-    @endcard
+            <div class="form-group mt-4">
+                <button type="submit">Crear usuario</button>
+                <a href="{{ route('users.index') }}" class="btn btn-link">Regresar al listado de usuarios</a>
+            </div>
+        </form>
+    @endcomponent
 @endsection
